0af13cb800236f15f858f39489b77f54a370bd21
sahilpatel123 <sahilpatel@wustl.edu>
2022-02-07 22:09:36 -0800

    Ready for algo

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
-//add unlisted coin button
+
+
 function calculate() {
     // If submit button clicked
     $("#calculator-button").click(function () {
       let input1 = document.getElementById("var-input-1").value
+      let v1 = parseInt(input1)
       let input2 = document.getElementById("var-input-2").value
+      let v2 = parseInt(input2)
       let input3 = document.getElementById("var-input-3").value
+      let v3 = parseInt(input3)
       let input4 = document.getElementById("var-input-4").value
-      console.log(input1, input2, input3, input4)
-      console.log("here")
-      answer = input1 + input2 + input3 + input4
-      console.log(answer)
+      let v4 = parseInt(input4)
+      answer =  v1 + v2 + v3 + v4
       $("#soln").html(answer);
   });
   // If they hit the enter key
   $("#var-input-4").keypress(function (e) {
       if (e.which == 13) {
         let input1 = document.getElementById("var-input-1").value
+        let v1 = parseInt(input1)
         let input2 = document.getElementById("var-input-2").value
+        let v2 = parseInt(input2)
         let input3 = document.getElementById("var-input-3").value
+        let v3 = parseInt(input3)
         let input4 = document.getElementById("var-input-4").value
-        console.log(input1, input2, input3, input4)
-        console.log("here")
-        e.preventDefault();
-        answer = input1 + input2 + input3 + input4
+        let v4 = parseInt(input4)
+        answer =  v1 + v2 + v3 + v4
         $("#soln").html(answer);
+        e.preventDefault();
       }
   }); 
 }
